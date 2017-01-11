57cf2ec5ff9b2cae19b7270be39efbb3804358f9
Saleh Pirouzi <salehpirouzi@gmail.com>
2017-01-11 10:37:07 +0000

    Adding a javascript file to the project

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Adam Smith";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+    };
+
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+    };
+})();
